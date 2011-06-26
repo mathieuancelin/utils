3eb279d1dd2a68453cf239b24990217326e0d8ea
Mathieu ANCELIN <mathieu.ancelin@gmail.com>
2011-06-26 15:00:37 +0200

    Add first support for remote actors through HTTP

--- src/main/java/cx/ath/mancel01/utils/Actors.java
+++ src/main/java/cx/ath/mancel01/utils/Actors.java
@@ -1,7 +1,23 @@
 package cx.ath.mancel01.utils;
 
+import com.sun.net.httpserver.HttpExchange;
+import com.sun.net.httpserver.HttpHandler;
+import com.sun.net.httpserver.HttpServer;
 import cx.ath.mancel01.utils.C.Function;
 import cx.ath.mancel01.utils.F.Option;
+import java.io.BufferedReader;
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.io.OutputStreamWriter;
+import java.net.InetSocketAddress;
+import java.net.URL;
+import java.net.URLConnection;
+import java.net.URLEncoder;
 import java.util.Iterator;
 import java.util.List;
 import java.util.UUID;
@@ -19,33 +35,119 @@ import java.util.concurrent.atomic.AtomicBoolean;
  * @author Mathieu ANCELIN
  */
 public class Actors {
-    
-    private static final ConcurrentHashMap<String, ActorRef> actors = 
+
+    private static final ConcurrentHashMap<String, ActorRef> actors =
             new ConcurrentHashMap<String, ActorRef>();
     
+    private static HttpServer remotingServer;
+    
+    private static final ExecutorService remotingServerExecutor = Executors.newCachedThreadPool();
+
     private static ActorRef getActor(String name) {
-        // here manage remote and local actors
         return actors.get(name);
     }
-    
+
     private static void register(String name, ActorRef actor) {
-        // here manage remote and local actors
         actors.putIfAbsent(name, actor);
     }
-    
+
     private static void unregister(String name) {
-        // here manage remote and local actors
         actors.remove(name);
     }
-    
+
     public static Option<ActorRef> forName(String name) {
         return Option.maybe(Actors.getActor(name));
     }
-    
+
     public static void shutdownAll() {
         Actor.executor.shutdownNow();
     }
-    
+
+    public static void startRemoting(String host, int port) {
+        try {
+            remotingServer = HttpServer.create(new InetSocketAddress(host, port), 0);
+            remotingServer.setExecutor(remotingServerExecutor);
+            remotingServer.createContext("/", new HttpHandler() {
+                @Override
+                public void handle(HttpExchange he) throws IOException {
+                    System.out.println("req ...");
+                    String body = slurpBody(he.getRequestBody());
+                    System.out.println(body);
+                    String[] params = body.split("&");
+                    String remoteName = "";
+                    String remoteFrom = "";
+                    String remoteHost = "";
+                    String remotePort = "";
+                    String remoteMsg = "";
+                    for (String param : params) {
+                        if (param.startsWith("remote-name")) {
+                            remoteName = param.replace("remote-name=", "");
+                        }
+                        if (param.startsWith("remote-from")) {
+                            remoteFrom = param.replace("remote-from=", "");
+                        }
+                        if (param.startsWith("remote-host")) {
+                            remoteHost = param.replace("remote-host=", "");
+                        }
+                        if (param.startsWith("remote-port")) {
+                            remotePort = param.replace("remote-port=", "");
+                        }
+                        if (param.startsWith("remote-msg")) {
+                            remoteMsg = param.replace("remote-msg=", "");
+                        }
+                    }
+                    Option<ActorRef> ref = forName(remoteName);
+                    if (ref.isDefined()) {
+                        try {
+                            ref.get().send(fromString(remoteMsg), 
+                                    "remote://" + remoteHost
+                                    + ":" + remotePort
+                                    + "/" + remoteFrom);
+                        } catch (Exception e) {
+                            throw new RuntimeException(e);
+                        }
+                    }
+                    he.sendResponseHeaders(200, 0);
+                    he.getResponseBody().flush();
+                    he.close();
+                    System.out.println("end req");
+                }
+                
+                private String slurpBody(InputStream body) {
+                    if (body == null)
+                        return "null";
+                    try {
+                        StringBuilder out = new StringBuilder();
+                        byte[] b = new byte[4096];
+                        for (int n; (n = body.read(b)) != -1;) {
+                            out.append(new String(b, 0, n));
+                        }
+                        return out.toString();
+                    } catch (IOException ex) {
+                        return "IO/error";
+                    }
+                }
+            });
+            remotingServer.start();
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public static ActorStore remote(final String host, final int port) {
+        return new ActorStore() {
+            @Override
+            public ActorRef forName(final String name) {
+                return new RemoteActorRef(name, host, port);
+            }
+        };
+    }
+
+    public static interface ActorStore {
+
+        ActorRef forName(String name);
+    }
+
     public static interface ActorRef {
 
         void send(Object msg);
@@ -61,7 +163,6 @@ public class Actors {
     public static abstract class Actor extends Thread implements LocalActorRef {
 
         private static final BlockingQueue<Runnable> tasks = new ArrayBlockingQueue<Runnable>(500);
-        
         private static ExecutorService executor = Executors.newCachedThreadPool();
 
         static {
@@ -71,15 +172,15 @@ public class Actors {
                     @Override
                     public void run() {
                         executor.shutdownNow();
+                        remotingServer.stop(0);
+                        remotingServerExecutor.shutdownNow();
                     }
-                }
-            );
+                });
         }
 
         private static class Message {
 
             Object payload;
-            
             Option<ActorRef> sender;
 
             public Message(Object payload, Option<ActorRef> sender) {
@@ -87,31 +188,28 @@ public class Actors {
                 this.sender = sender;
             }
         }
-        private ConcurrentLinkedQueue<Message> mailbox = 
+        private ConcurrentLinkedQueue<Message> mailbox =
                 new ConcurrentLinkedQueue<Message>();
-        
         private Function<Object> react;
-        
         private AtomicBoolean started = new AtomicBoolean(false);
-        
         private AtomicBoolean buzy = new AtomicBoolean(false);
-        
         protected Option<ActorRef> sender = Option.none();
-        
         protected final String uuid;
 
         public Actor() {
             uuid = UUID.randomUUID().toString();
             Actors.register(uuid, this);
         }
-        
+
         public void unregister() {
             Actors.unregister(uuid);
         }
 
-        protected void before() {}
-    
-        protected void after() {}
+        protected void before() {
+        }
+
+        protected void after() {
+        }
 
         @Override
         public void run() {
@@ -189,10 +287,19 @@ public class Actors {
             Option<ActorRef> opt = Option.maybe(from);
             mailbox.add(new Message(msg, opt));
         }
-        
+
         @Override
         public final void send(Object msg, String from) {
-            send(msg, Actors.getActor(from));
+            if (from.startsWith("remote://")) {
+                String url = from.replace("remote://", "");
+                String host = url.split(":")[0];
+                String rest = url.split(":")[1];
+                String port = rest.split("/")[0];
+                String name = rest.split("/")[1];
+                send(msg, new RemoteActorRef(name, host, Integer.valueOf(port)));
+            } else {
+                send(msg, Actors.getActor(from));
+            }
         }
 
         public final Actor stopActor() {
@@ -206,11 +313,58 @@ public class Actors {
             return this;
         }
     }
+    
+    public static class RemoteActorRef implements ActorRef {
+        
+        private final String host;
+        
+        private final int port;
+        
+        private final String name;
+
+        public RemoteActorRef(String name, String host, int port) {
+            this.host = host;
+            this.port = port;
+            this.name = name;
+        }
+        
+        @Override
+        public void send(Object msg) {
+            sendToRemoteActor(msg, "blackhole");
+        }
+
+        @Override
+        public void send(Object msg, String from) {
+            sendToRemoteActor(msg, from);
+        }
+
+        private void sendToRemoteActor(Object msg, String from) {
+            try {
+                if (!msg.getClass().equals(String.class)) {
+                    throw new RuntimeException("can't send anything than string");
+                }
+                String data = URLEncoder.encode("remote-name", "UTF-8") + "=" + URLEncoder.encode(name, "UTF-8");
+                data += "&" + URLEncoder.encode("remote-msg", "UTF-8") + "=" + URLEncoder.encode(Actors.toString(msg), "UTF-8");
+                data += "&" + URLEncoder.encode("remote-from", "UTF-8") + "=" + URLEncoder.encode(from, "UTF-8");
+                data += "&" + URLEncoder.encode("remote-host", "UTF-8") + "=" + URLEncoder.encode(remotingServer.getAddress().getHostName(), "UTF-8");
+                data += "&" + URLEncoder.encode("remote-port", "UTF-8") + "=" + URLEncoder.encode(remotingServer.getAddress().getPort() + "", "UTF-8");                            
+                URL url = new URL("http://" + host + ":" + port + "/");
+                URLConnection conn = url.openConnection();
+                conn.setDoOutput(true);
+                OutputStreamWriter wr = new OutputStreamWriter(conn.getOutputStream());
+                wr.write(data);
+                wr.flush();
+                conn.connect();
+                wr.close();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+    }
 
     public static class LoadBalancer {
 
         private final List<Actor> actors;
-        
         private Iterator<Actor> it;
 
         public LoadBalancer(List<Actor> actors) {
@@ -263,11 +417,11 @@ public class Actors {
             }
         }
     }
-    
+
     public static class Broadcaster {
 
         private final List<Actor> actors;
-        
+
         public Broadcaster(List<Actor> actors) {
             this.actors = actors;
         }
@@ -331,6 +485,22 @@ public class Actors {
     }
 
     public static class PoisonPill {}
-    
+
     public static class Kill {}
+    
+    private static Object fromString(String s) 
+            throws IOException ,ClassNotFoundException {
+        byte [] data = s.getBytes("UTF-8");
+        ObjectInputStream ois = new ObjectInputStream(new ByteArrayInputStream(data));
+        Object o  = ois.readObject();
+        ois.close();
+        return o;
+    }
+    private static String toString(Object o) throws IOException {
+        ByteArrayOutputStream baos = new ByteArrayOutputStream();
+        ObjectOutputStream oos = new ObjectOutputStream(baos);
+        oos.writeObject(o);
+        oos.close();
+        return new String(baos.toByteArray());
+    }
 }
